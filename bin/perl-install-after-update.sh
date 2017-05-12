7e0a232192176d0f6b2b1ede7ef1fb2ccfb05f27
slegga3 <sslegga@gmail.com>
2017-05-12 15:28:45 +0200

    Added files for perl updates after new os

---
+++ bin/perl-install-after-update.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cpan YAML
+cpan Git::Repository
+cpan IO::Socket::IP
+cpan YAML::Tiny
+cpan Log::Log4perl
+cpan Mojolicious::Plugin::OpenAPI
---
+++ t/ok.t
@@ -0,0 +1,2 @@
+use Test::More;
+ok(1==1,"Dummy test");
